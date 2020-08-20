e1ff92e407404147da24b0c4f2f89d6c8ada785f
romerodf <69981320+romerodf@users.noreply.github.com>
2020-08-20 12:35:41 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Dimayri>");
+		System.out.println("<Hello Java Project>");
+		System.out.println("<java program>");
+		System.out.println("<August 20>");
+		System.out.println("<CMSC 255 Section 001>");
+		System.out.println();
+	}
+}
